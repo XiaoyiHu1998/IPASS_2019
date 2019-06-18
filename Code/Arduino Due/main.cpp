097a50eee69d00025cfd62f4e35af4cf957744bc
Xiao yi Hu <xy1998@live.nl>
2019-06-18 12:37:21 +0200

    added test cpp file

---
+++ Code/Arduino Due/main.cpp
@@ -0,0 +1,24 @@
+#include "hwlib.hpp"
+namespace target = hwlib::target;
+
+int main(){
+    char input;
+    auto pin = target::pin_out(target::pins::d13);
+
+    while(1){
+        hwlib::cin >> input;
+        if(input = 'a'){
+            for(int i  0; i < 5; i++){
+                Pin.write(1);
+                pin.flush();
+                hwlib::wait_ms(200);
+                pin.write(0);
+                pin.flush()
+                hwlib::wait_ms(200);
+            }
+            
+        }
+        hwlib::wait_ms(100);
+    }
+    
+}
