dfbdea8fc49b5579952c3f3d36001f1aac538eb8
Xiao yi Hu <xy1998@live.nl>
2019-06-19 15:21:27 +0200

    Compile errors fixed

--- Code/PC/Linux/serialPort_linux.cpp
+++ Code/PC/Linux/serialPort_linux.cpp
@@ -1,12 +1,11 @@
-#include "serialPort.h"
+#include "serialPort_linux.h"
 
-serialPort_linux(std::string & path){
+serialPort_linux::serialPort_linux(char* path){
     port = open(path, O_RDWR);
 
     //check for errors
     if(port < 0){
         printf("ERROR %i from open: %s\n", errno, strerror(errno));
-        ~serialPort_linux();
     }
 
     //create termios struct called tty
@@ -25,30 +24,30 @@ serialPort_linux(std::string & path){
 
     //read in existing settings into termios struct
     if(tcgetattr(port, &tty) != 0){
-        printf("Error %i from tcgetattr while reading in existing values: %s\n", errno, streerno(errno));
+        printf("Error %i from tcgetattr while reading in existing values: %s\n", errno, strerror(errno));
     }
     //control flags
     tty.c_cflag |= PARENB;          //clears parity bit, disabling it (more common)
     tty.c_cflag &= ~CSTOPB;         //clears stop field, only one stop bit
-    tty.c_cflag |= CS8              //8 bits per byte
+    tty.c_cflag |= CS8  ;            //8 bits per byte
     tty.c_cflag &= ~CRTSCTS;        //disables hardware RTS/CTS control flow
-    tty.c_clfag |= CREAD | CLOCAL;  //turns on read and ignores ctrl lines (Clocal = 1)
+    tty.c_cflag |= CREAD | CLOCAL;  //turns on read and ignores ctrl lines (Clocal = 1)
     
     //local flags
-    tty.c_lflags &= ~ICANON;    //sets non-canonical mode
-    tty.c_lflags &= ECHO;       //disables echo
-    tty.c_lflags &= ECHOE;      //disables erasure
-    tty.c_lflags &= ECHONL;     //disable newline echo
+    tty.c_lflag &= ~ICANON;    //sets non-canonical mode
+    tty.c_lflag &= ECHO;       //disables echo
+    tty.c_lflag &= ECHOE;      //disables erasure
+    tty.c_lflag &= ECHONL;     //disable newline echo
     tty.c_lflag &= ~ISIG;       //disables INTR, QUIT and SUSP
 
     //input flags
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY));                                        //disables s/w flow control
-    tty.c_ilfag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INCLR | IGNCR | ICRNL);    //disable handling of recieved bytes
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);                                        //disables s/w flow control
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);    //disable handling of recieved bytes
 
     //output mode flags
     tty.c_oflag &= ~OPOST; //prevent special interpretaion of bytes
-    tty.c_oflag &= ~ONCLR; //prevents conversion of newline to carriage return/line feed
-    tty.c_oflag &= ~ONOET; //prevent removal of C d chars (0x004) in input
+    tty.c_oflag &= ~ONLCR; //prevents conversion of newline to carriage return/line feed
+    // tty.c_oflag &= ~ONOET; //prevent removal of C d chars (0x004) in input
 
     //VMIN and VTIME (c_cc)
     tty.c_cc[VMIN] &= 2;
@@ -60,15 +59,15 @@ serialPort_linux(std::string & path){
     
     //save attributes for tty
     if(tcsetattr(port, TCSANOW, &tty) != 0){
-        printf("ERROR %i from tcsetattr: %s\n", errno, strerrror(errno)); //error message
+        printf("ERROR %i from tcsetattr: %s\n", errno, strerror(errno)); //error message
     }
 }
 
-~serialPort_linux(){
+serialPort_linux::~serialPort_linux(){
     close(port);
 }
 
-bool serialPort_linux::write(const void *message, const size_t size){
+bool serialPort_linux::writeData(const void* message, const size_t size){
     int bytesWritten = write(port, message, size);
 
     if(bytesWritten < 0){
@@ -79,15 +78,15 @@ bool serialPort_linux::write(const void *message, const size_t size){
     }
 }
 
-char* serialPort_linux::read(const size_t toRead){
+void serialPort_linux::readData(char* & readBuffer, const size_t toRead){
     //allocate memmory for readBuffer
-    char readBuffer[2];
+    char buffer[2];
     memset(&readBuffer, '\0', sizeof(readBuffer));
 
-    int n = read(port, &readBuffer, sizeof(readBuffer));
-    if(n < 0){
+    int amountRead = read(port, &readBuffer, sizeof(readBuffer));
+    if(amountRead < 0){
         std::cout << "ERROR: error while reading serial port" << std::endl;
     }
 
-    return readBuffer;
+    readBuffer = buffer;
 }
--- Code/PC/Linux/test/serialPort_linux_test.cpp
+++ Code/PC/Linux/test/serialPort_linux_test.cpp
@@ -1,1 +1,15 @@
-#include "serialPort_linux.h"
+#include "../serialPort_linux.h"
+#include "string.h"
+
+int main(){
+    char path[13] = "/dev/ttyUSB0";
+    char character = 'c';
+    // std::cout << "Please enter the path of of your serial port: ";
+    // std::cin >> path;
+    serialPort_linux port(path);
+
+    while(1){
+        std::cout << port.writeData("a", 1);
+        sleep(2);
+    }
+}
