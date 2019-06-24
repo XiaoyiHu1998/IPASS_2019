77a6a751e40f4e455e0815e1561065b3e44f8699
Xiao yi Hu <xy1998@live.nl>
2019-06-24 17:47:02 +0200

    changed the way that parameters for functions are recieved

--- Code/Arduino Due/main.cpp
+++ Code/Arduino Due/main.cpp
@@ -1,69 +1,83 @@
 #include "hwlib.hpp"
+#include "duePins.h"
+
 namespace target = hwlib::target;
- 
-// Pio & __attribute__((weak)) port_registers( uint32_t port ){
-   
-//    // a bit of a hack to put this here:
 
-//    // kill the watchdog
-//    WDT->WDT_MR = WDT_MR_WDDIS;
+target::pin_in_out pins[54] = {
+                                    target::pin_in_out(target::pins::d0),
+                                    target::pin_in_out(target::pins::d1),
+                                    target::pin_in_out(target::pins::d2),
+                                    target::pin_in_out(target::pins::d3),
+                                    target::pin_in_out(target::pins::d4),
+                                    
+                                    target::pin_in_out(target::pins::d5),
+                                    target::pin_in_out(target::pins::d6),
+                                    target::pin_in_out(target::pins::d7),
+                                    target::pin_in_out(target::pins::d8),
+                                    target::pin_in_out(target::pins::d9),
+                                    
+                                    target::pin_in_out(target::pins::d10),
+                                    target::pin_in_out(target::pins::d11),
+                                    target::pin_in_out(target::pins::d12),
+                                    target::pin_in_out(target::pins::d13),
+                                    target::pin_in_out(target::pins::d14),
+                                    
+                                    target::pin_in_out(target::pins::d15),
+                                    target::pin_in_out(target::pins::d16),
+                                    target::pin_in_out(target::pins::d17),
+                                    target::pin_in_out(target::pins::d18),
+                                    target::pin_in_out(target::pins::d19),
+                                    
+                                    target::pin_in_out(target::pins::d20),
+                                    target::pin_in_out(target::pins::d21),
+                                    target::pin_in_out(target::pins::d22),
+                                    target::pin_in_out(target::pins::d23),
+                                    target::pin_in_out(target::pins::d24),
+                                    
+                                    target::pin_in_out(target::pins::d25),
+                                    target::pin_in_out(target::pins::d26),
+                                    target::pin_in_out(target::pins::d27),
+                                    target::pin_in_out(target::pins::d28),
+                                    target::pin_in_out(target::pins::d29),
+                                    
+                                    target::pin_in_out(target::pins::d30),
+                                    target::pin_in_out(target::pins::d31),
+                                    target::pin_in_out(target::pins::d32),
+                                    target::pin_in_out(target::pins::d33),
+                                    target::pin_in_out(target::pins::d34),
+                                    
+                                    target::pin_in_out(target::pins::d35),
+                                    target::pin_in_out(target::pins::d36),
+                                    target::pin_in_out(target::pins::d37),
+                                    target::pin_in_out(target::pins::d38),
+                                    target::pin_in_out(target::pins::d39),
+                                    
+                                    target::pin_in_out(target::pins::d40),
+                                    target::pin_in_out(target::pins::d41),
+                                    target::pin_in_out(target::pins::d42),
+                                    target::pin_in_out(target::pins::d43),
+                                    target::pin_in_out(target::pins::d44),
+                                    
+                                    target::pin_in_out(target::pins::d45),
+                                    target::pin_in_out(target::pins::d46),
+                                    target::pin_in_out(target::pins::d47),
+                                    target::pin_in_out(target::pins::d48),
+                                    target::pin_in_out(target::pins::d49),
+                                    
+                                    target::pin_in_out(target::pins::d50),
+                                    target::pin_in_out(target::pins::d51),
+                                    target::pin_in_out(target::pins::d52),
+                                    target::pin_in_out(target::pins::d53)
+                                };
 
-//    // enable the clock to all GPIO ports
-//    PMC->PMC_P2CER0 = ( 0x3F << 11 );  
+duePins duePins(pins);
 
-//    switch( port ){
-//       case 0  : return *PIOA;
-//       case 1  : return *PIOB;
-//       case 2  : return *PIOC;
-//       case 3  : return *PIOD;
-//       default : break;
-//    }  
-   
-//    // doesn't return   
-//    HWLIB_PANIC_WITH_LOCATION; 
-// }
+char getNextParamter(){
+    char parameter;
+    hwlib::cin >> parameter;
 
-void setPinIO(bool io){
-    char portNumberChar;
-    char pinChar;
-    hwlib::cin >> portNumberChar;
-    hwlib::cin >> pinChar;
-
-    uint8_t portNumber = static_cast<uint8_t> (portNumberChar);
-    uint8_t pin = static_cast<uint8_t> (pinChar);
-
-    Pio & port = portNumber;
-    uint32_t mask = 0x1U << pin;
-
-    (io ? port.PIO_ODR : port.PIO_OER) = mask;
+    return parameter;
 }
-
-void setOutput(){
-    char portNumberChar;
-    char pinChar;
-    char ioChar;
-    hwlib::cin >> portNumberChar;
-    hwlib::cin >> pinChar;
-    hwlib::cin >> ioChar;
-
-    uint8_t portNumber = static_cast<uint8_t> (portNumberChar);
-    uint8_t pin = static_cast<uint8_t> (pinChar);
-    uint8_t io = static_cast<uint8_t> (ioChar);
-
-    Pio & port = portNumber;
-    uint32_t mask = 0x1U << pin;
-
-    //set ouptut pin high
-    if(io == '1'){
-        port.SODR = mask;
-    }
-    //set output pin low
-    else if(io == '0'){
-        port.CODR = mask;
-    }
-}
-
-void flush(){}
 
 int main(){
     while(1){
@@ -100,25 +114,92 @@ int main(){
                 //     case '4':
 
                 // }
+
             //pin_in_out
             case '5':
+                int pinNumber = 0;
+
+                switch(function){
+                    //setOutput()
+                    case '1':
+                        for(unsigned int i = 0; i < 6; i++){
+                            char value = getNextParamter();
+                            
+                            pinNumber += int (value);
+                        }
+
+                        duePins.setOutput(pinNumber);
+
+                    //setInput()
+                    case '2':
+                        for(unsigned int i = 0; i < 6; i++){
+                            char value = getNextParamter();
+                            
+                            pinNumber += int (value);
+                        }
+
+                        duePins.setInput(pinNumber);
+
+                    //write()
+                    case '3':
+                        for(unsigned int i = 0; i < 6; i++){
+                            char value = getNextParamter();
+                            
+                            pinNumber += int (value);
+                        }
+
+                        if(getNextParamter() == '1'){
+                            duePins.write(pinNumber, 1);
+                        }
+                        else{
+                            duePins.write(pinNumber, 0);
+                        }
+
+                    //read()
+                    case '4':
+                        for(unsigned int i = 0; i < 6; i++){
+                            char value = getNextParamter();
+                            
+                            pinNumber += int (value);
+                        }
+
+                        hwlib::cout << duePins.read(pinNumber);
+
+                    //flush()
+                    case '5':
+                        for(unsigned int i = 0; i < 6; i++){
+                            char value = getNextParamter();
+                            
+                            pinNumber += int (value);
+                        }
+
+                        duePins.flush(pinNumber);
+
+                    //refresh()
+                    case '6':
+                        for(unsigned int i = 0; i < 6; i++){
+                            char value = getNextParamter();
+                            
+                            pinNumber += int (value);
+                        }
+
+                        duePins.refresh(pinNumber);
+                }
+
             //pin_oc
             case '6':
             //pin_out
             case '7':
-                switch(function){
-                    //Constructor call, enable pin as output
-                    case 'c':
-                        setPinIO(0);
+                // switch(function){
+                //     //Constructor call, enable pin as output
+                //     case 'c':
 
-                    //pin_out.write()
-                    case '1':
-                        setOutput();
+                //     //pin_out.write()
+                //     case '1':
 
-                    //pin_out.flush()
-                    case '2':
-                        flush();
-                }
+                //     //pin_out.flush()
+                //     case '2':
+                // }
             case '8':
             case '9':
             case 'a':
