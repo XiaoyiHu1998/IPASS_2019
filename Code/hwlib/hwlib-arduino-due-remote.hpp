79bf1fa18dff97246be32ddf607be8c9a3595374
Xiao yi Hu <xy1998@live.nl>
2019-07-03 17:33:23 +0200

    updated code for port_outs and tests

--- Code/hwlib/hwlib-arduino-due-remote.hpp
+++ Code/hwlib/hwlib-arduino-due-remote.hpp
@@ -4,6 +4,8 @@
 #include "/home/xiaoyi/Desktop/IPASS_2019/Code/PC/Linux/serialPort_linux.hpp"
 #include "/home/xiaoyi/Desktop/IPASS_2019/Code/PC/Linux/serialPort_linux_path.hpp"
 
+//======================================================================
+
 serialPort_linux serialPort(path);
 
 //======================================================================
@@ -45,6 +47,71 @@ class pin_adc{
    virtual void refresh() = 0;
 };
 
+
+// ==========================================================================
+//
+// input / output port
+//
+// ==========================================================================
+
+
+class port_in_out {
+public:
+
+   virtual uint_fast8_t number_of_pins() = 0;
+   
+   virtual void direction_set_input() = 0;   
+   
+   virtual uint_fast16_t read() = 0;         
+   
+   virtual void refresh() = 0;
+
+   virtual void direction_set_output() = 0;
+
+   virtual void write( uint_fast16_t x ) = 0;     
+   
+   virtual void flush() = 0;  
+   
+   virtual void direction_flush() = 0;
+
+};
+   
+   
+// ==========================================================================
+//
+// input port
+//
+// ==========================================================================
+
+class port_in {
+public:
+
+   virtual uint_fast8_t number_of_pins() = 0;
+   
+   virtual uint_fast16_t read() = 0; 
+
+   virtual void refresh() = 0;
+
+};
+
+
+// ==========================================================================
+//
+// output port
+//
+// ==========================================================================
+
+class port_out {
+public:
+
+   virtual uint_fast8_t number_of_pins() = 0;
+   
+   virtual void write( uint_fast16_t x ) = 0;     
+   
+   virtual void flush() = 0;      
+
+};
+
 };
 
 //======================================================================
@@ -54,7 +121,8 @@ namespace due_remote{
 //======================================================================
 
 void waitForStartSignal(){
-   std::cout << "waiting for startSignal" << std::endl << std::endl;
+   std::cout << "Serialport: " << path << std::endl;
+   std::cout << "waiting for start signal from arduino" << std::endl << std::endl;
    char startupSignal;
    for(;;){
       startupSignal = serialPort.readChar();
@@ -367,7 +435,9 @@ public:
 
 //======================================================================
 
+pin_in_out_dummy in_in_out_dummy;
 pin_out_dummy in_out_dummy;
+pin_in_dummy in_in_dummy;
 
 class all_from_pin_out_t : due_remote_primitives::pin_out{
 private:
@@ -415,6 +485,257 @@ public:
       }
    }
 };
+
+
+class all_from_port_out_t : due_remote_primitives::pin_out{
+private:
+   due_remote_primitives::port_out & slave;
+public:
+   all_from_port_out_t(
+                        due_remote_primitives::port_out & slave
+                     ):
+                        slave(slave)
+                     {}
+   
+   void write(bool v) override{
+      slave.write( v ? 0xFF : 0x0 );
+   }
+
+   void flush() override{
+      slave.flush();
+   }
+};
+
+
+//======================================================================
+
+class port_in_out_from_pins_t : public due_remote_primitives::port_in_out{
+private:
+   static constexpr int max_pins = 16;
+ 
+   uint_fast8_t _number_of_pins;  
+ 
+   due_remote_primitives::pin_in_out * pins[ max_pins ]; 
+
+public:
+   port_in_out_from_pins_t(
+                              due_remote_primitives::pin_in_out & p0 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p1 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p2 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p3 = in_in_out_dummy,
+                              
+                              due_remote_primitives::pin_in_out & p4 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p5 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p6 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p7 = in_in_out_dummy,
+                              
+                              due_remote_primitives::pin_in_out & p8 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p9 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p10 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p11 = in_in_out_dummy,
+
+                              due_remote_primitives::pin_in_out & p12 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p13 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p14 = in_in_out_dummy,
+                              due_remote_primitives::pin_in_out & p15 = in_in_out_dummy
+                           ):
+                           pins{
+                              &p0, &p1, &p2, &p3,
+                              &p4, &p5, &p6, &p7,
+                              &p8, &p9, &p10, &p11,
+                              &p12, &p13, &p14, &p15
+                           }
+                           {
+                              for(_number_of_pins = 0; _number_of_pins < max_pins; ++_number_of_pins){
+                                 if(pins[_number_of_pins] == & in_in_out_dummy){
+                                    break;
+                                 }
+                              }
+                           }
+
+   uint_fast8_t number_of_pins() override{
+      return _number_of_pins;
+   }
+   
+   void direction_set_input() override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->direction_set_input();
+      }
+   } 
+   
+   uint_fast16_t read() override{
+      uint_fast8_t result = 0;
+      for(uint_fast8_t i = _number_of_pins - 1; i >= 0; --i){
+         result = result << 1;
+         if(pins[i]->read()){
+            result |= 0x01;
+         }
+      }
+
+      return result;
+   }         
+   
+   void refresh() override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->refresh();
+      }
+   }  
+
+   void direction_set_output() override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->direction_set_output();
+      }
+   }  
+   void write( uint_fast16_t x ) override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->write((x & 0x01) != 0);
+         x = x >> 1;
+      }
+   }      
+   
+   void flush() override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->flush();
+      }
+   }   
+   
+   void direction_flush()  override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->direction_flush();
+      }
+   }  
+};
+
+
+class port_out_from_pins_t : public due_remote_primitives::port_out{
+private:
+   static constexpr int max_pins = 16;
+ 
+   uint_fast8_t _number_of_pins;  
+ 
+   due_remote_primitives::pin_out * pins[ max_pins ]; 
+
+public:
+   port_out_from_pins_t(
+                        due_remote_primitives::pin_out & p0 = in_out_dummy,
+                        due_remote_primitives::pin_out & p1 = in_out_dummy,
+                        due_remote_primitives::pin_out & p2 = in_out_dummy,
+                        due_remote_primitives::pin_out & p3 = in_out_dummy,
+                        
+                        due_remote_primitives::pin_out & p4 = in_out_dummy,
+                        due_remote_primitives::pin_out & p5 = in_out_dummy,
+                        due_remote_primitives::pin_out & p6 = in_out_dummy,
+                        due_remote_primitives::pin_out & p7 = in_out_dummy,
+                        
+                        due_remote_primitives::pin_out & p8 = in_out_dummy,
+                        due_remote_primitives::pin_out & p9 = in_out_dummy,
+                        due_remote_primitives::pin_out & p10 = in_out_dummy,
+                        due_remote_primitives::pin_out & p11 = in_out_dummy,
+
+                        due_remote_primitives::pin_out & p12 = in_out_dummy,
+                        due_remote_primitives::pin_out & p13 = in_out_dummy,
+                        due_remote_primitives::pin_out & p14 = in_out_dummy,
+                        due_remote_primitives::pin_out & p15 = in_out_dummy
+                     ):
+                     pins{
+                        &p0, &p1, &p2, &p3,
+                        &p4, &p5, &p6, &p7,
+                        &p8, &p9, &p10, &p11,
+                        &p12, &p13, &p14, &p15
+                     }
+                     {
+                        for(_number_of_pins = 0; _number_of_pins < max_pins; ++_number_of_pins){
+                           if(pins[_number_of_pins] == & in_out_dummy){
+                              break;
+                           }
+                        }
+                     }
+
+   uint_fast8_t number_of_pins() override{
+      return _number_of_pins;
+   }
+
+   void write( uint_fast16_t x ) override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->write((x & 0x01) != 0);
+         x = x >> 1;
+      }
+   }      
+   
+   void flush() override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->flush();
+      }
+   }
+};
+
+class port_in_from_pins_t : public due_remote_primitives::port_in{
+private:
+   static constexpr int max_pins = 16;
+ 
+   uint_fast8_t _number_of_pins;  
+ 
+   due_remote_primitives::pin_in * pins[ max_pins ]; 
+
+public:
+   port_in_from_pins_t(
+                        due_remote_primitives::pin_in & p0 = in_in_dummy,
+                        due_remote_primitives::pin_in & p1 = in_in_dummy,
+                        due_remote_primitives::pin_in & p2 = in_in_dummy,
+                        due_remote_primitives::pin_in & p3 = in_in_dummy,
+                        
+                        due_remote_primitives::pin_in & p4 = in_in_dummy,
+                        due_remote_primitives::pin_in & p5 = in_in_dummy,
+                        due_remote_primitives::pin_in & p6 = in_in_dummy,
+                        due_remote_primitives::pin_in & p7 = in_in_dummy,
+                        
+                        due_remote_primitives::pin_in & p8 = in_in_dummy,
+                        due_remote_primitives::pin_in & p9 = in_in_dummy,
+                        due_remote_primitives::pin_in & p10 = in_in_dummy,
+                        due_remote_primitives::pin_in & p11 = in_in_dummy,
+
+                        due_remote_primitives::pin_in & p12 = in_in_dummy,
+                        due_remote_primitives::pin_in & p13 = in_in_dummy,
+                        due_remote_primitives::pin_in & p14 = in_in_dummy,
+                        due_remote_primitives::pin_in & p15 = in_in_dummy
+                     ):
+                     pins{
+                        &p0, &p1, &p2, &p3,
+                        &p4, &p5, &p6, &p7,
+                        &p8, &p9, &p10, &p11,
+                        &p12, &p13, &p14, &p15
+                     }
+                     {
+                        for(_number_of_pins = 0; _number_of_pins < max_pins; ++_number_of_pins){
+                           if(pins[_number_of_pins] == & in_in_dummy){
+                              break;
+                           }
+                        }
+                     }
+
+   uint_fast8_t number_of_pins() override{
+      return _number_of_pins;
+   }
+   
+   uint_fast16_t read() override{
+      uint_fast8_t result = 0;
+      for(uint_fast8_t i = _number_of_pins - 1; i >= 0; --i){
+         result = result << 1;
+         if(pins[i]->read()){
+            result |= 0x01;
+         }
+      }
+
+      return result;
+   }         
+   
+   void refresh() override{
+      for(uint_fast8_t i = 0; i < _number_of_pins; i++){
+         pins[i]->refresh();
+      }
+   }
+};
+
 
 //======================================================================
 
